2b1b68d1caee3617dbac5819957827ce30d4d8a6
opokuwarejunior <opokuwarejunior4@gmail.com>
2025-10-18 21:04:58 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,190 @@
+# print("Hello everyone this is Junior")
+# print('Today is 10 octber 2025')
+
+# # string manipulation 
+
+# first_name = "Nana Opoku Ware "
+# Last_name = "Junior"
+# title_name = "Getting Serious this Three Month "
+# Course_name = " Sql , Python , Mainframe "
+# Month = 'October , Novembr , December'
+
+# #string concatenation 
+# Advance_course = title_name + '\n' + first_name
+# Course_name = Month + '\n' + first_name
+
+# print(Course_name)
+# print(Last_name)
+
+
+
+# # f string 
+# name = 'Nana Opoku Ware Junior'
+# age = '19 years old '
+# role = 'Sql Instrutor , Python Instrutor, Data Enginer'
+
+# print(f"My name is {name}")
+# print (f"I am {age}years old")
+# print (f"I am {role} at Adupslam company" )
+
+# int = 90
+# int = type(int)
+# print(int)
+
+# type_of_string = type("I am a boy") 
+# print(type_of_string)
+# bill = 90.99
+# type_of_bill = type(bill)
+# # print(type_of_bill)
+
+'''
+This lecture is called data type casting /
+python casting , data coversion 
+simplely means convering int to a str 
+
+'''
+twitter_ceo = 'Elon musk'
+twitter_worth ='7_000_000_000'
+age = 34
+cost = 1945.77
+
+# int_cost = str(cost)
+# print(type(int_cost))
+
+# int_age = int(age)
+# print(int_age)
+
+# int_cost = int(cost)
+# print(type(int_cost) )
+
+# is_nana = True
+
+# # str_is_nana = str(is_nana)
+# # print(type(str_is_nana))
+# float_age = float(age)
+# print(type(float_age))
+
+# Name = input ('what is your name'  )
+# Dream_Job = input('what is your dream Job')
+# Expectations = input('Next three month neeed to be good at')
+# is_nana = input('Is nana a good student True or False')
+
+
+
+
+
+# print(is_nana)
+# print(Name)
+# print(Dream_Job)
+# print(Expectations)
+
+
+'''
+python challenge 
+'''
+
+# name = input('what is your name')
+# company = input('what is your dream company')
+# programming_language = input('what is your favorite language')
+
+# solutions = f"I am {name} , I  wish to work at my  {company}  and  My favorite programming language is {programming_language} . "
+
+# print(solutions)
+
+
+
+
+
+# student_name = ('Sophia Chen')
+# print(student_name)
+
+# math_score = '65'
+# int_average_score = int(65) 
+# print(int_average_score)
+
+# reading_score = '72'
+# int_average_score = int(72)
+# print(int_average_score)
+
+# science_score = '68'
+# int_Average_score = int(68)
+# print(int_Average_score)
+
+# total_score = (68 + 72 + 65)
+# print(total_score)
+
+# average_score = (205) / 3
+# print(average_score)
+
+# average_score = f'The average_score is {average_score:.2f}'
+# print(average_score)
+
+
+
+
+
+'''
+Question two and solution 
+'''
+
+# student_name = ('Noah Garcai')
+# print(student_name)
+
+# math_score = '78'
+# reading_score = '82' 
+# Science_score = '75'
+
+# int_math_score= int(78)
+# int_reading_score = int(82)
+# int_science_score = int(75)
+
+# print(int_math_score)
+# print(int_reading_score)
+# print(int_science_score)
+
+
+# total_score = (78 + 82 + 75 )
+# print(total_score)
+
+# Average_score = 235 / 3
+# print(Average_score)
+
+
+# Average_score = f'The Average_score is {Average_score:.2f}'
+# print(Average_score)
+
+
+
+
+'''
+Questions 3
+'''
+
+student_name = ('Jacob Smith')
+print(student_name)
+
+int_math_score = int(100)
+int_reading_score = int(100)
+int_science_score = int(100)
+
+print(int_math_score)
+print(int_reading_score)
+print(int_science_score)
+
+total_score = (100 + 100 + 100)
+print(total_score)
+
+
+Average_score = 300 /3
+print(Average_score)
+
+Average_score = f'The average_score is {Average_score:2f}'
+print(Average_score)
+
+# This should create: "Welcome John Smith! Your member since 2022."
+first_name = "john"
+last_name = "smith"
+member_year = "2022"
+
+message = f"Welcome {first_name.capitalize} {last_name.capitalize}! You've been a member since {member_year}."
+print(message)
